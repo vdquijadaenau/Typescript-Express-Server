4aaf5e5659a9d2051aa6366e9952ae2c1a2e524c
vdquijadaenau <vdquijadaenau@gmail.com>
2022-04-15 16:28:55 +1000

    decoratoras added

---
+++ src/controllers/decorators/routes.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
+  };
+}
+
+export const get = routeBinder("get");
+export const put = routeBinder("put");
+export const post = routeBinder("post");
+export const del = routeBinder("del");
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -15,22 +15,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction) {
   res.send("No permitted");
 }
 
-router.get("/login", (req: Request, res: Response) => {
-  res.send(`
-  <form method="POST">
-    <div>
-        <label>Email:</label>
-        <input name="email"/>
-    </div>
-    <div>
-        <label>Password:</label>
-        <input name="password"/>
-    </div>
-    <button>Submit</button>
-  </form>
-  `);
-});
-
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
   if (email && password && email === "g@g.com" && password === "12345678") {
